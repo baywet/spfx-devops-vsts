3cee88ef5279cbbf1ba0325ad8832d3ac05aa243
Vincent Biret <vincentbiret@hotmail.com>
2017-03-27 23:32:38 -0400

    adding business service and unit tests

--- src/webparts/devOpsWebPart/tests/DevOpsWebPart.test.ts
+++ src/webparts/devOpsWebPart/tests/DevOpsWebPart.test.ts
@@ -1,9 +1,34 @@
 /// <reference types="mocha" />
+/// <reference types="chai-as-promised" />
 
 import { assert } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { DevOpsWebPartService } from '../DevOpsWebPart.service';
 
 describe('DevOpsWebPartWebPart', () => {
   it('should do something', () => {
     assert.ok(true);
+  });
+});
+let service = new DevOpsWebPartService();
+describe('DevOpsWebPartWebPart', () => {
+  it('should add numbers Sync fluent', () => {
+    let result = service.add(1, 3);
+    return result.should.equal(4); // fluent API
+  });
+});
+
+describe('DevOpsWebPartWebPart', () => {
+  it('should add numbers sync assert', () => {
+    let result = service.add(1, 3);
+    assert.equal(result, 4, "result equals 4"); // assert/imperative API
+  });
+});
+
+chai.use(chaiAsPromised);
+describe('DevOpsWebPartWebPart', () => {
+  it('should add numbers async', () => {
+    let asyncResult = service.addAsync(1, 3);
+    return asyncResult.should.eventually.equal(4); // npm install chai-as-promised @types/chai-as-promised -D
   });
 });
