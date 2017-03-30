6ba70be771d529f8c4fda8ce6571f45b8171b868
Vincent Biret <vincentbiret@hotmail.com>
2017-03-30 18:55:36 -0400

    adding support for code coverage

--- config/karma.config.js
+++ config/karma.config.js
@@ -2,15 +2,32 @@
 var existingKarmaConfig = require('@microsoft/sp-build-web/lib/karma/karma.config');
 var _ = require('lodash');
 var junitReporter = require('karma-junit-reporter');
+var remapIstanbulReporter = require('karma-remap-istanbul');
+var remapCoverageReporter = require('karma-remap-coverage');
 
 module.exports = function (config) {
-    existingKarmaConfig(config);
-    config.reporters.push('junit');
-    config.junitReporter = {
-      outputDir: 'temp/', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'test-results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
-      suite: 'karma', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true, // add browser name to report and classes names
-    };
-    config.plugins.push(junitReporter);
+  existingKarmaConfig(config);
+  config.reporters.push('junit');
+  config.junitReporter = {
+    outputDir: 'temp/', // results will be saved as $outputDir/$browserName.xml
+    outputFile: 'test-results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+    suite: 'karma', // suite will become the package name attribute in xml testsuite element
+    useBrowserName: true, // add browser name to report and classes names
+  };
+  var coverageReportsRoot = './coverage';
+  var coberturaSubDir = 'cobertura';
+  var coberturaFileName = 'cobertura.xml';
+  config.coverageReporter.reporters.push({type: 'cobertura', subdir: './' + coberturaSubDir, file: coberturaFileName});
+  config.remapIstanbulReporter = {
+    reports: {
+      cobertura: coverageReportsRoot + '/' + coberturaSubDir + '/' + coberturaFileName,
+    }
+  };
+  remapCoverageReporter = {
+    cobertura: coverageReportsRoot + '/' + coberturaSubDir + '/' + coberturaFileName,
+  };
+
+  config.plugins.push(remapIstanbulReporter);
+  config.plugins.push(remapCoverageReporter);
+  config.plugins.push(junitReporter);
 };
