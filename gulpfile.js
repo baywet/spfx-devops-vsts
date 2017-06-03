0ef4da809213692cbb3b459ef623a20e1e2c53ae
Vincent Biret <vincentbiret@hotmail.com>
2017-06-03 09:51:06 +0200

    integrating elios improvements

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,31 @@ const spsync = require('gulp-spsync-creds').sync;
 const argv = require('yargs').argv;
 
 
-build.task('upload-to-sharepoint', { 
-    execute: (config) => {
-        return new Promise((resolve, reject) => {
-            const deployFolder = require('./config/copy-assets.json');
-            const folderLocation = `./${deployFolder.deployCdnPath}/**/*.js*`;
-            return gulp.src(folderLocation)
-            .pipe(spsync({
-                "username": argv.username,
-                "password": argv.password,
-                "site": argv.targetUrl,
-                "libraryPath": "SiteAssets/spfx",
-                "publish": true
-            }))
-            .on('finish', resolve);
-        });
-    }
+build.task('upload-to-sharepoint', {
+  execute: (config) => {
+    if (config.production) {
+      environment.username =  config.production ? config.args['username'] || "";
+      environment.password = config.args['password'] || "";
+      environment.tenant = config.args['tenant'] || "";
+      environment.cdnSite = config.args['cdnsite'] || "";
+      environment.cdnLib = config.args['cdnlib'] || "";
+    }
+
+    return new Promise((resolve, reject) => {
+      const deployFolder = require('./config/copy-assets.json');
+      const folderLocation = `./${deployFolder.deployCdnPath}/**/*.js`;
+
+      return gulp.src(folderLocation)
+        .pipe(spsync({
+          "username": environment.username,
+          "password": config.environment ? environment.password : development.password,
+          "site": `https://${config.environment ? environment.tenant : development.tenant}.sharepoint.com/${config.environment ? environment.cdnSite : development.cdnSite}`,
+          "libraryPath": config.environment ? environment.cdnLib : development.cdnLib,
+          "publish": true
+        }))
+        .on('finish', resolve);
+    });
+  }
 });
 
 build.initialize(gulp);
